2d09ddeab7179a890ee614f0c8f3d40ce26d576f
Stephen Bach <bach@cs.umd.edu>
2013-03-05 17:38:27 -0500

    Added PSL installation.

---
+++ install_psl.sh
@@ -0,0 +1,3 @@
+mvn install:install-file -Dfile=psl-core.jar -DgroupId=edu.umd.cs -DartifactId=psl-core -Dversion=1.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=psl-groovy.jar -DgroupId=edu.umd.cs -DartifactId=psl-groovy -Dversion=1.1-SNAPSHOT -Dpackaging=jar
+
