9a260c14d3702c6a4a36f28f0eab55b2894d8875
Stephen Bach <bach@cs.umd.edu>
2013-03-06 14:43:22 -0500

    Added to PSL snapshot.

--- install_psl.sh
+++ install_psl.sh
@@ -1,2 +1,3 @@
-mvn install:install-file -Dfile=psl-core-1.1-SNAPSHOT.jar -DgroupId=edu.umd.cs -DartifactId=psl-core -Dversion=1.1-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile=psl-groovy-1.1-SNAPSHOT.jar -DgroupId=edu.umd.cs -DartifactId=psl-groovy -Dversion=1.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=psl-1.1-SNAPSHOT/psl-1.1-SNAPSHOT.pom.xml -DgroupId=edu.umd.cs -DartifactId=psl -Dversion=1.1-SNAPSHOT -Dpackaging=pom
+mvn install:install-file -Dfile=psl-1.1-SNAPSHOT/psl-core-1.1-SNAPSHOT.jar -DpomFile=psl-1.1-SNAPSHOT/psl-core-1.1-SNAPSHOT.pom.xml -DgroupId=edu.umd.cs -DartifactId=psl-core -Dversion=1.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=psl-1.1-SNAPSHOT/psl-groovy-1.1-SNAPSHOT.jar -DpomFile=psl-1.1-SNAPSHOT/psl-groovy-1.1-SNAPSHOT.pom.xml -DgroupId=edu.umd.cs -DartifactId=psl-groovy -Dversion=1.1-SNAPSHOT -Dpackaging=jar
