e241c985451eb5d7d7571b8332876d04340fb19f
Bert Huang <bert@cs.umd.edu>
2013-03-06 13:06:07 -0500

    changed name of jars in install_psl.sh

--- install_psl.sh
+++ install_psl.sh
@@ -1,3 +1,2 @@
-mvn install:install-file -Dfile=psl-core.jar -DgroupId=edu.umd.cs -DartifactId=psl-core -Dversion=1.1-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile=psl-groovy.jar -DgroupId=edu.umd.cs -DartifactId=psl-groovy -Dversion=1.1-SNAPSHOT -Dpackaging=jar
-
+mvn install:install-file -Dfile=psl-core-1.1-SNAPSHOT.jar -DgroupId=edu.umd.cs -DartifactId=psl-core -Dversion=1.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=psl-groovy-1.1-SNAPSHOT.jar -DgroupId=edu.umd.cs -DartifactId=psl-groovy -Dversion=1.1-SNAPSHOT -Dpackaging=jar
