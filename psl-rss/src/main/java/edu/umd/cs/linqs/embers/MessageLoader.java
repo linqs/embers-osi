269262f1acd1b53e4227d6229d1c822d05d7e696
Bert Huang <bert@cs.umd.edu>
2013-05-13 13:46:03 -0400

    changed version tag to geocode_version

--- psl-rss/src/main/java/edu/umd/cs/linqs/embers/MessageLoader.java
+++ psl-rss/src/main/java/edu/umd/cs/linqs/embers/MessageLoader.java
@@ -184,7 +184,7 @@ public class MessageLoader {
 		embersGeoCode.put("country", country);
 		embersGeoCode.put("admin1", state);
 		embersGeoCode.put("city", city);
-		embersGeoCode.put("psl_geocode_version", "PSL 5-6-13");
+		embersGeoCode.put("geocode_version", "PSL 5-6-13");
 		json.put("embersGeoCode", embersGeoCode);
 	}
 
